c2e8df31e3b335dcae7cdab4747d233f20426357
DESKTOP-PN1JS55\wangchao <593504602@qq.com>
2018-01-09 17:35:10 +0800

    ex40 commit

---
+++ ex40.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2018/1/9 16:34
+# @Author : wangchao
+# @Site : 
+# @File : ex40.py
+# @Software: PyCharm
+
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print line
+
+happy_bday = Song(["Happy birthday to you",
+                   "I don't want to get sued",
+                   "So I'll stop right here"])
+bulls_on_parade = Song(["They really around the family",
+                        "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+bulls_on_parade.sing_me_a_song()
